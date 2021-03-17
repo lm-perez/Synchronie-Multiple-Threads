60f5b5062d8a9b125b78db2964378fdc4a9a6854
L.M. Perez <79484518+lm-perez@users.noreply.github.com>
2021-03-16 21:34:17 -0400

    -

---
+++ build/Debug/Cygwin-Windows/lab6.o.d
@@ -0,0 +1,1 @@
+build/Debug/Cygwin-Windows/lab6.o: lab6.c
---
+++ main.c
@@ -0,0 +1,106 @@
+/**
+ *
+ * COE 628 - Operating Systems
+ * Lab 6
+ * Lianne Perez
+ **/
+
+// if not already compiled run: (wont build in netbeans)
+// gcc -pthread -o main main.c
+// else if the main executable is already in folder you can just run it
+#include <stdlib.h>     /* NULL */
+#include  <stdio.h>	/* printf */
+#include  <sys/types.h>	/* pid_t */
+#include <unistd.h>	/* get_pid */
+#include <stdlib.h>     /* exit, EXIT_FAILURE */
+#include <sys/wait.h> 	/* wait */
+#include <pthread.h>
+
+#define PRODUCER_NO 5	//Number of producers, can change here
+#define NUM_PRODUCED 20 //2000 //Number of items to be produced, can change here
+
+void *generator_function(void*);
+void *print_function(void*);
+long sum; /* Sum of generated values*/
+long finished_producers; /* number of the producer that finished producing */
+
+//C: Mutex declaration and initialization
+pthread_mutex_t mutex1 = PTHREAD_MUTEX_INITIALIZER; //mutex for generator function
+pthread_mutex_t mutex2 = PTHREAD_MUTEX_INITIALIZER;
+
+//F: Condition variable declaration and initialization
+pthread_cond_t condition = PTHREAD_COND_INITIALIZER;
+
+
+int main(void) {
+    int i;
+    /* initialize random seed: */
+    srand(time(NULL));
+    sum = 0;
+    finished_producers=0;
+    
+    //A: Creates five generator thread
+    pthread_t generator_thread[5];
+    
+    for(i = 0; i < 5; i++){
+        pthread_create(&generator_thread[i], NULL, &generator_function, NULL);
+    }
+    //D: Creates print thread
+    pthread_t print_thread;
+    pthread_create(&print_thread, NULL, &print_function, NULL);
+    
+    //B: Makes sure that all generator threads has finished before proceeding
+    for(i = 0; i < 5; i++){
+        pthread_join(generator_thread[i], NULL);
+    }
+    //E: Makes sure that print thread has finished before proceeding
+    pthread_join(print_thread, NULL);
+
+    return (0);
+
+}
+
+void * generator_function(void* junk) {
+    pthread_mutex_lock(&mutex1); //lock
+    long counter = 0;
+    long sum_this_generator = 0;
+   
+    while (counter < NUM_PRODUCED) {
+      
+      
+      long tmpNumber = sum;
+      long rnd_number = 1;//rand() % 10; 
+        // can make = 1, output should be 100 (20 loops*5 threads)
+      	printf("current sum of the generated number up to now is %ld going to add %ld to it.\n", tmpNumber, rnd_number);
+        sum = tmpNumber + rnd_number;
+        counter++;
+        sum_this_generator += rnd_number;
+        usleep(1000);
+    }
+       
+    printf("--+---+----+----------+---------+---+--+---+------+----\n");
+    printf("The sum of produced items for this number generator at the end is: %ld \n", sum_this_generator);
+    printf("--+---+----+----------+---------+---+--+---+------+----\n");
+    finished_producers++;
+    pthread_mutex_unlock(&mutex1); // unlock
+    
+    //H: If all generator has finished fire signal for condition variable
+    pthread_mutex_lock(&mutex2);//lock mutex for condition signal, locked by calling thread to prevent undefined behavior.
+    pthread_cond_signal(&condition);//wakes printer;
+    pthread_mutex_unlock(&mutex2);
+    
+    return (0);
+}
+
+void *print_function(void* junk) {
+    //G: Wait until all generator has finished
+    while(finished_producers < 5){
+        pthread_cond_wait(&condition, &mutex2);
+    }
+    
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    printf("The value of counter at the end is: %ld \n", sum);
+    printf("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n");
+    
+    return 0;
+}
